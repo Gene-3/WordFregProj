cb6dd033e0c5ee6de537ef4712bdd90d3fb81527
Gene-3 <rhkd352@naver.com>
2026-04-10 22:24:22 +0900

    Add files via upload

--- WordFreqWebDashboard.py
+++ WordFreqWebDashboard.py
@@ -1,30 +1,50 @@
+import streamlit as st
 from mylib import myTextAnalyzer as ta
-
-# 데이터 로딩
-data_filename = '.\data\daum_movie_review.csv'
-column = 'review'
-corpus = ta.load_corpus_from_csv(data_filename, column)
-#print(corpus[:5])
-
-# 토큰화 -> 빈도수 추출
+from mylib import myStreamlitVisualizer as sv
 from konlpy.tag import Okt
-tokenizer = Okt().pos
-my_tags = ['Noun', 'Verb', 'Adjective']
-my_stopwords = ['영화', '이', '더', '잘', '것', '점', '좀', '그', '입니다', '하는', '보고', '수', '그냥', '정도', '돈', '할']
-tokens = ta.tokenizer_korean_corpus(corpus, tokenizer, my_tags, my_stopwords)
-#print(tokens[:10])
 
-counter = ta.analyze_word_freq(tokens)
-#print(list(counter.items())[:10])
+sv.main()
 
-# 시각화
-# 1. 수평 막대그래프
-num_words = 20
-title = '영화 리뷰'
-ylabel = '키워드'
-xlabel = '빈도수'
-font_path = 'c:/Windows/Fonts/malgun.ttf'
-ta.visualize_barhgraph(counter, num_words, title, xlabel, ylabel, font_path)
+with st.sidebar :
+    uploaded_file = st.file_uploader("파일 선택", type = ['csv'])
 
-# 2. 워드클라우드
-ta.visualize_wordcloud(counter, num_words, font_path)
+    sv.whether_file(uploaded_file)
+
+    params = None
+    my_stopwords = []
+    title = None
+    ylabel = None
+    if uploaded_file :
+        params = sv.set_parameter()
+        my_stopwords = st.text_area("불용어를 입력하세요",'').split()
+        st.subheader("워드클라우드")
+        title = st.text_input("타이틀을 입력해주세요")
+        ylabel = st.text_input("키워드를 입력해주세요")
+    else : 
+        st.warning("파일을 첨부해주세요")
+
+
+if uploaded_file and params and params["column_name"]:
+    target_column = params.get("column_name")
+
+    counter = None
+    if target_column :
+        corpus = ta.load_corpus_from_csv(uploaded_file, params['column_name'])
+        tokenizer = Okt().pos
+        my_tags = ['Noun', 'Verb', 'Adjective']
+        num_words = 20
+        font_path = 'c:/Windows/Fonts/malgun.ttf'
+
+        with st.spinner("빈도수 그래프 생성중...") :
+            tokens = ta.tokenizer_korean_corpus(corpus, tokenizer, my_tags, my_stopwords)
+            counter = ta.analyze_word_freq(tokens)
+        
+    if params.get("bar_words") :
+        xlabel = '빈도수'
+        ta.visualize_barhgraph(counter, num_words, title, xlabel, ylabel, font_path)
+        
+        
+    if params.get("word_cloud") :
+            ta.visualize_wordcloud(counter, num_words, font_path)
+if uploaded_file and params and not params["column_name"]:
+    st.warning("컬럼명을 입력하세요")
